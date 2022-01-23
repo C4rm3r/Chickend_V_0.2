c3a4ffbeaa0352b1a69159c16a4634eda8b514e8
udim12 <ehud6401@gmail.com>
2022-01-23 17:33:45 +0200

    redesigned the homepage
    Signed-off-by: udim12 <ehud6401@gmail.com>

--- app/src/main/java/EMA/chickend/MainActivity.java
+++ app/src/main/java/EMA/chickend/MainActivity.java
@@ -3,27 +3,17 @@ package EMA.chickend;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import EMA.chickend.GUI.AllLevelsOverviewForm;
-import EMA.chickend.Logic.Classes.AppUtils;
-import EMA.chickend.Logic.Classes.Game;
-import EMA.chickend.Logic.Classes.Level;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,12 +23,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         m_context = getApplicationContext();
 
         setContentView(R.layout.activity_main);
 
+        RelativeLayout layout = findViewById(R.id.RelativeLayout_Layout);
+        if (layout != null) {
+            layout.setBackgroundResource(R.drawable.background___ancient_egypt);
+            layout.getBackground().setAlpha(150);
+        }
+        ImageView chick = findViewById(R.id.chick_chicken_start);
+        ImageView regularChicken = findViewById(R.id.regular_chicken_start);
+        ImageView motherChicken = findViewById(R.id.mother_chicken_start);
+
+        motherChicken.getBackground().setAlpha(230);
+        chick.getBackground().setAlpha(230);
+        regularChicken.getBackground().setAlpha(230);
+
+
+
         Button btn = findViewById(R.id.mybutton);
+
+        btn.setAlpha(1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
